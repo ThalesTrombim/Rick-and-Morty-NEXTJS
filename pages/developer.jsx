f4fdce92be23c12cf7c312fe797e6405576e07fd
ThalesTrombim <thalestr@outlook.com>
2021-12-28 22:20:36 -0300

    pagina de contato/dev finalizada

---
+++ pages/developer.jsx
@@ -0,0 +1,26 @@
+import { Header } from '../src/components/Header';
+
+export default function Developer() {
+    return (
+        <div id='container' className='bg-homebg-dark h-screen'>
+            <Header />
+
+            <main className='mt-24 md:mt-44 flex flex-col items-center text-2xl md:text-3xl text-white gap-6'>
+                <div className='w-60 h-60 md:w-80 md:h-80 bg-white rounded-full bg-cover bg-center' style={{backgroundImage: `url('images/dev.jpg')`}}></div>
+                <h3>Thales Trombim</h3>
+                <div className='flex gap-10'>
+                    <a href="https://github.com/ThalesTrombim" target="_blank" >
+                        <img className='w-8 md:w-10 md:h-10' src="/images/icons/github.png" alt="email" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/thalestrombim/" target="_blank" >
+                        <img className='w-8 md:w-10 md:h-10' src="/images/icons/linkedin.png" alt="email" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/thalestrombim/" target="_blank" >
+                        <img className='w-8 md:w-10 md:h-10' src="/images/icons/email.png" alt="email" />
+                    </a>
+                </div>
+            </main>
+
+        </div>
+    )
+}
