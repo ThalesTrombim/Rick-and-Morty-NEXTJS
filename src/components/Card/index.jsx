2506751194fa587f61b78082c3b54222d6cdb68d
ThalesTrombim <thalestr@outlook.com>
2022-01-25 19:55:50 -0300

    ajuste no card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,35 +1,28 @@
-
-
-
-const round_icons = "bg-gradient-to-r from-blue-fim via-blue-meio  to-blue-comeco to-blue-comeco w-12 h-12 rounded-full flex";
-
 export default function Card({ character }){
     return (
-        <>
-            <a key={character.id} href={`/character/${character.id}`} className="">
-                <div className="bg-cover w-68 pb-3 bg-center bg-card-bg rounded-xl mt-5 md:mt-0">
-                    <div className="pt-3">
-                        <img className="w-11/12 h-56 m-auto rounded-xl" src={character.image} alt={character.name} />
-                        <div className="w-11/12 m-auto rounded-xl pb-3 px-2 pt-2 border-2 border-blue-400 mt-3">
-                            <h3 className="font-teste text-2xl text-center">{character.name}</h3>
-                            <p className="text-center text-sm">{character.type}</p>
-                            <div className="flex justify-between pt-3 items-center">
-                                <div className="flex flex-col items-center">
-                                    <img className="w-8 h-8" src={`/images/${character.species}.png`} alt={character.species} />
-                                    <p>{character.species}</p>
-                                </div>
-                                <div className="flex flex-col items-center">
-                                    <img className="w-8 h-8" src={`/images/${character.status}.png`} alt={character.status} />
-                                    <p>{character.status}</p>
-                                </div>
-                                <button className="bg-blue-500 h-10 py-1 px-3 rounded-full">
-                                    Read more
-                                </button>
-                            </div>
+        <a key={character.id} href={`/character/${character.id}`} className='h-xl bg-card-bg rounded-xl w-72'>
+            <div className='w-full p-3 h-full flex flex-col justify-between'>
+                <img className="w-full h-56 rounded-xl" src={character.image} alt={character.name} />
+                <div className='flex flex-col items-center gap-5'>
+                    <div className='flex flex-col items-center'>
+                        <span className='font-medium text-2xl'>{ character.name }</span>
+                        <span>{ character.species}</span>
+                    </div>
+                    <div className='flex justify-between w-full'>
+                        <div className="flex flex-col items-center">
+                            <img className="w-8 h-8" src={`/images/${character.species}.png`} alt={character.species} />
+                            <p>{character.species}</p>
                         </div>
+                        <div className="flex flex-col items-center">
+                            <img className="w-8 h-8" src={`/images/${character.status}.png`} alt={character.status} />
+                            <p>{character.status}</p>
+                        </div>
+                        <button className="bg-blue-500 h-10 py-1 px-3 rounded-full">
+                            Read more
+                        </button>
                     </div>
                 </div>
-            </a>
-        </>
+            </div>
+        </a>
     )
 }
