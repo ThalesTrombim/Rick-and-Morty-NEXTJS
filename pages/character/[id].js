a1fd3d5b00be4387899503986b41ae1e52272665
ThalesTrombim <thalestr@outlook.com>
2022-03-06 17:15:18 -0300

    começando ajustes na pagina de personagem

--- pages/character/[id].js
+++ pages/character/[id].js
@@ -10,16 +10,16 @@ export default function CharacterItem({ info }) {
 
     return (
         <div className="w-full md:h-screen bg-cover bg-center p-2 md:px-11 flex items-center justify-center" style={{ backgroundImage: `url(${data[`${name}`].image})` }}>
-            <div className="md:flex bg-white bg-white md:py-14 rounded-xl justify-around align-center">
-                <div>
+            <div className="md:flex bg-white md:py-14 rounded-xl justify-around align-center">
+                <div className='bg-blue-500'>
                     <img className="md:w-auto w-full rounded-xl" src={info.image} />
                 </div>
                 <div className="grid md:w-1/2 p-2 md:p-0 gap-1">
                     <h2 className="md:text-7xl text-2xl text-purple font-bold">{info.name}</h2>
                     <div className="flex md:gap-4 text-sm items-center">
-                        <p className="border-2 rounded-xl p-1">Gênero: {info.gender}</p>
+                        <p className="border-2 rounded-xl p-1">Gender: {info.gender}</p>
                         <p className="border-2 rounded-xl p-1">Status: {info.status}</p>
-                        <p className="border-2 rounded-xl p-1">Espécie: {info.species}</p>
+                        <p className="border-2 rounded-xl p-1">Species: {info.species}</p>
                     </div>
                     <p className={data[`${name}`].text} >{data[`${name}`].description}</p>
                 </div>
@@ -30,8 +30,11 @@ export default function CharacterItem({ info }) {
 }
 
 export async function getServerSideProps(context) {
-    const res = await fetch(`https://rick-and-morty-nextjs-pearl.vercel.app/api/character/${context.params.id}`);
+    const id = context.params.id;
+    const res = await fetch(`https://rick-and-morty-nextjs-pearl.vercel.app/api/character/${id}`);
     const json = await res.json()
+    
+    const infos_desc = await fetch(``);
 
     return {
         props: {
