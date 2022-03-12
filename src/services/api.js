7e75c30e9f9d6bffce641cd0da3a1a211feb92f4
ThalesTrombim <thalestr@outlook.com>
2022-03-12 16:41:19 -0300

    passando os cookies em todas a requisições http

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
+import { parseCookies } from 'nookies';
+
+const { 'randm.token': token } = parseCookies();
 
 export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND,
 })
+
+if(token) {
+    api.defaults.headers['Authorization'] = `Bearer ${token}`;
+}
