e31f2d6748be5877bffb514fa35cfc65baa68e4c
thales <thalestr@outlook.com>
2023-11-09 18:20:15 -0300

    feat: update detail character page

--- pages/character/[id].js
+++ pages/character/[id].js
@@ -1,29 +1,22 @@
 import React from 'react';
-import data from '../../data';
 
 export default function CharacterItem({ character }) {
-
-    var name = character.name;
-
-    if (!data[`${name}`]) {
-        name = "Default";
-    }
+    const backgroundImageName = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
 
     return (
-        <div className="h-screen w-full md:h-screen bg-cover bg-center p-6 md:px-11 flex items-center justify-center" style={{ backgroundImage: `url(${data[`${name}`].image})` }}>
-            <div className="md:flex bg-white md:py-14 rounded-xl justify-around align-center">
+        <div className="h-screen w-full md:h-screen bg-cover bg-center p-6 md:px-11 flex items-center justify-center" style={{backgroundImage: `url('/images/backgrounds/detailsPage/image-${backgroundImageName}.jpg')`}} >
+            <div className="md:flex bg-white align-center justify-between gap-3">
                 <div className=''>
-                    <img className="md:w-auto w-full rounded-t-xl md:rounded-xl" src={character.image} />
+                    <img className="md:w-auto w-full" src={character.image} />
                 </div>
-                <div className="grid md:w-1/2 p-2 md:p-0 gap-1">
-                    <h2 className="md:text-7xl text-2xl text-purple font-bold">{character.name}</h2>
-                    <div className="flex md:gap-4 text-sm items-center">
-                        <p className="border-2 rounded-xl p-1">Gender: {character.gender}</p>
-                        <p className="border-2 rounded-xl p-1">Status: {character.status}</p>
-                        <p className="border-2 rounded-xl p-1">Species: {character.species}</p>
+                <div className="grid p-2 md:p-3 md:pt-12 gap-1 md:w-2/3 flex-wrap">
+                    <h2 className="md:text-2xl text-xl text-purple font-bold">{character.name}</h2>
+                    <div className="flex text-sm items-center justify-between my-12 md:mb-0">
+                        <p className=""><strong>Status:</strong> {character.status}</p>
+                        <p className=""><strong>Origin:</strong> {character.origin.name}</p>
+                        <p className=""><strong>Species:</strong> {character.species}</p>
                     </div>
                 </div>
-
             </div>
         </div>
     );
